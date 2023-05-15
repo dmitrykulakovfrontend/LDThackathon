74e53cdfc03399d9606f32c4761ae1a5a1916f96
Dmitry Kulakov <atomeistee@gmail.com>
2023-05-15 12:06:32 +0300

    initial commit

---
+++ src/main/java/com/cringeneers/LDThackathon/LdThackathonApplication.java
@@ -0,0 +1,13 @@
+package com.cringeneers.LDThackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdThackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdThackathonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cringeneers/LDThackathon/LdThackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cringeneers.LDThackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdThackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

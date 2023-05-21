c8046c9c1afdd6502218bd241ad9e68e3106007a
noradrenal1ne <vassanor@gmail.com>
2023-05-21 14:54:07 +0300

    cross origin fix #2

--- src/main/java/com/cringeneers/LDThackathon/controller/AuthController.java
+++ src/main/java/com/cringeneers/LDThackathon/controller/AuthController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 
-@CrossOrigin("http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {

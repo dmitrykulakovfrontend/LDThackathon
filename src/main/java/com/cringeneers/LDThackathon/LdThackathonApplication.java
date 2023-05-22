2df29bf7d24711d9932e62e892865d25d7d66a1e
noradrenal1ne <vassanor@gmail.com>
2023-05-22 19:28:44 +0300

    fix 1

--- src/main/java/com/cringeneers/LDThackathon/LdThackathonApplication.java
+++ src/main/java/com/cringeneers/LDThackathon/LdThackathonApplication.java
@@ -2,9 +2,11 @@ package com.cringeneers.LDThackathon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-public class LdThackathonApplication {
+public class LdThackathonApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LdThackathonApplication.class, args);

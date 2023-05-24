bbeac03c2460bf806e7849e17c75c943e8fbf63e
noradrenal1ne <vassanor@gmail.com>
2023-05-24 13:21:51 +0300

    add user auth validation

--- src/main/java/com/cringeneers/LDThackathon/entity/User.java
+++ src/main/java/com/cringeneers/LDThackathon/entity/User.java
@@ -1,6 +1,10 @@
 package com.cringeneers.LDThackathon.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 import java.util.Set;
@@ -16,9 +20,15 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @NotBlank
     private String name;
+    @NotBlank
     private String username;
+    @Email(message = "Некорректная почта!", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
+    @NotEmpty(message = "Поле с почтой не может быть пустым!")
     private String email;
+
+    @Min(8)
     private String password;
 
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)

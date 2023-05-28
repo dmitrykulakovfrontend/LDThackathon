c95ebab2471c08093f31297353e45d888b293cb8
Dmitry Kulakov <atomeistee@gmail.com>
2023-05-28 14:30:40 +0300

    CRUD for equipment

--- src/client/src/routes/admin/data/equipment.tsx
+++ src/client/src/routes/admin/data/equipment.tsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import EditIcon from "@/assets/edit.svg";
+import DeleteIcon from "@/assets/trashcan.svg";
+import { API_URL } from "@/constants";
 
 const headers = ["№", "Наименование", "Код", "Цена", "Действия"];
-const equipments = [
+const equipments: {
+  [key: string]: string;
+}[] = [
   {
-    id: 1,
+    id: "1",
     name: "Стандарт",
     code: "S001",
     price: "1000",
   },
   {
-    id: 2,
+    id: "2",
     name: "Стандарт 2",
     code: "S002",
     price: "2000",
@@ -18,9 +23,19 @@ const equipments = [
 
 function Equipment() {
   // function name(params: type) {}
+  const [isCreating, setIsCreating] = useState(false);
   return (
     <div className="mt-5 overflow-x-auto">
-      <table className="border-collapse border-ldt-gray">
+      <div className="flex items-center justify-between">
+        <h1 className="my-8 text-3xl font-bold">Средняя цена оборудования</h1>
+        <button
+          onClick={() => setIsCreating(true)}
+          className="px-12 py-2 text-blue-500 border-2 border-blue-500 h-fit rounded-xl"
+        >
+          Добавить
+        </button>
+      </div>
+      <table className="w-full border-collapse border-ldt-gray">
         <thead className="border-2 border-ldt-gray">
           <tr>
             {headers.map((header, i) => (
@@ -34,6 +49,20 @@ function Equipment() {
           </tr>
         </thead>
         <tbody className="border-2 border-ldt-gray">
+          {isCreating ? (
+            <EquipmentDisplay
+              equipment={{
+                id: "",
+                name: "",
+                code: "",
+                price: "",
+              }}
+              isCreating
+              setIsCreating={setIsCreating}
+            />
+          ) : (
+            ""
+          )}
           {equipments.map((equipment, i) => (
             <EquipmentDisplay key={i} equipment={equipment} />
           ))}
@@ -44,15 +73,86 @@ function Equipment() {
   );
 }
 type Equipment = typeof equipments extends readonly (infer T)[] ? T : never;
-function EquipmentDisplay({ equipment }: { equipment: Equipment }) {
+function EquipmentDisplay({
+  equipment,
+  isCreating,
+  setIsCreating,
+}: {
+  equipment: Equipment;
+  isCreating?: boolean;
+  setIsCreating: (value: boolean) => void;
+}) {
+  function handleDelete() {
+    console.log("delete", equipment);
+  }
+  function handleEdit() {
+    setIsEditMode(true);
+    console.log("editing...");
+  }
+  function handleChange(key: string, value: string) {
+    setNewEquipment({
+      ...newEquipment,
+      [key]: value,
+    });
+  }
+  function handleSave() {
+    setIsEditMode(false);
+    console.log("save");
+  }
+  async function handleCreate() {
+    try {
+      const res = await fetch(`${API_URL}/admin/equipment`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          cost: newEquipment.price,
+          type: newEquipment.name,
+        }),
+      });
+      const data = await res.text();
+      console.log(data);
+      console.log("created");
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setIsCreating(false);
+    }
+  }
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [newEquipment, setNewEquipment] = useState(equipment);
   return (
     <>
       <tr className="border-2 border-ldt-gray">
-        {Object.values(equipment).map((value, i) => (
+        {Object.entries(equipment).map(([key, value], i) => (
           <td key={i} className="p-3 border-2 border-ldt-gray">
-            {value}
+            {isEditMode || isCreating ? (
+              <input
+                value={newEquipment[key]}
+                onChange={(e) => handleChange(key, e.target.value)}
+                type="text"
+                className="table-cell w-full border border-black"
+              ></input>
+            ) : (
+              value
+            )}
           </td>
         ))}
+        <td className="flex items-center gap-2 p-3">
+          <div className="hover:cursor-pointer" onClick={handleDelete}>
+            <DeleteIcon />
+          </div>
+          {isEditMode ? (
+            <span onClick={handleSave}>Сохранить</span>
+          ) : isCreating ? (
+            <span onClick={handleCreate}>Добавить</span>
+          ) : (
+            <div className="hover:cursor-pointer" onClick={handleEdit}>
+              <EditIcon />
+            </div>
+          )}
+        </td>
       </tr>
     </>
   );
